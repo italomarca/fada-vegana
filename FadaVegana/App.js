246618d400b7e3b1c6d92cc6ace5c10b67b3761e
Italo Marca <italomarca@icloud.com>
2020-02-11 00:42:25 -0300

    implements static product list

--- FadaVegana/App.js
+++ FadaVegana/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-} from 'react-native';
+import {StyleSheet, View, Text, ScrollView} from 'react-native';
 
-import { TextInput, ListView } from '@shoutem/ui';
+import {TextInput} from '@shoutem/ui';
 
 class App extends React.Component {
   constructor(ctx, props) {
@@ -14,88 +10,183 @@ class App extends React.Component {
     this.state = {
       searchText: '',
       products: [
-        { 
+        {
+          id: 1,
           name: 'Feijão',
           description: '',
           options: [
             {
               optionNumber: 1,
-              brand: 'Nestle'
+              brand: 'Inoar 1',
             },
             {
               optionNumber: 2,
-              brand: 'Dia'
+              brand: 'Dia',
             },
             {
               optionNumber: 3,
-              brand: 'Sei lá'
-            }
-          ]
+              brand: 'Sei lá',
+            },
+          ],
         },
-        { 
+        {
+          id: 2,
           name: 'Massa',
           description: '',
           options: [
             {
               optionNumber: 1,
-              brand: 'Nestle'
+              brand: 'Inoar 2',
             },
             {
               optionNumber: 2,
-              brand: 'Dia'
+              brand: 'Dia',
             },
             {
               optionNumber: 3,
-              brand: 'Sei lá'
-            }
-          ]
-        }
-      ]
-    }
+              brand: 'Sei lá',
+            },
+          ],
+        },
+        {
+          id: 2,
+          name: 'Massa',
+          description: '',
+          options: [
+            {
+              optionNumber: 1,
+              brand: 'Inoar 2',
+            },
+            {
+              optionNumber: 2,
+              brand: 'Dia',
+            },
+            {
+              optionNumber: 3,
+              brand: 'Sei lá',
+            },
+          ],
+        },
+        {
+          id: 2,
+          name: 'Massa',
+          description: '',
+          options: [
+            {
+              optionNumber: 1,
+              brand: 'Inoar 2',
+            },
+            {
+              optionNumber: 2,
+              brand: 'Dia',
+            },
+            {
+              optionNumber: 3,
+              brand: 'Sei lá',
+            },
+          ],
+        },
+        {
+          id: 2,
+          name: 'Massa',
+          description: '',
+          options: [
+            {
+              optionNumber: 1,
+              brand: 'Inoar 2',
+            },
+            {
+              optionNumber: 2,
+              brand: 'Dia',
+            },
+            {
+              optionNumber: 3,
+              brand: 'Sei lá',
+            },
+          ],
+        },
+        {
+          id: 2,
+          name: 'Massa',
+          description: '',
+          options: [
+            {
+              optionNumber: 1,
+              brand: 'Inoar 2',
+            },
+            {
+              optionNumber: 2,
+              brand: 'Dia',
+            },
+            {
+              optionNumber: 3,
+              brand: 'Sei lá',
+            },
+          ],
+        },
+        {
+          id: 2,
+          name: 'Massa',
+          description: '',
+          options: [
+            {
+              optionNumber: 1,
+              brand: 'Inoar 2',
+            },
+            {
+              optionNumber: 2,
+              brand: 'Dia',
+            },
+            {
+              optionNumber: 3,
+              brand: 'Sei lá',
+            },
+          ],
+        },
+      ],
+    };
   }
 
   _renderRow = rowData => {
-    return(
+    return (
       <View style={styles.rowItemWraper}>
-        <Text style={styles.rowItem}>
-          {rowData.name}
-        </Text>
-        {
-          rowData.options && rowData.options.map(data => (
-            <React.Fragment>
-              <Text style={styles.rowItem}>
-                {`Opção ${data.optionNumber}`}
-              </Text>
-              <Text style={styles.rowItem}>
-                {`Marca ${data.brand}`}
-              </Text>
-            </React.Fragment>
-          ))
-        }
+        <View style={styles.rowItemInnerWraper}>
+          <Text style={styles.productDescription}>{rowData.name}</Text>
+          {rowData.options &&
+            rowData.options.map(data => (
+              <View style={styles.brandOption}>
+                <Text style={styles.brandOptionText}>{`Opção ${data.optionNumber}`}</Text>
+                <Text style={styles.brandOptionText}>{`Marca: ${data.brand}`}</Text>
+              </View>
+            ))}
+        </View>
       </View>
-    )
-  }
+    );
+  };
 
   render() {
-    const { products } = this.state;
+    const {products} = this.state;
     return (
       <View style={styles.container}>
         <View style={styles.header}>
           <TextInput
             placeholder="Digite aqui seu produtinho"
             onChangeText={text => this.setState({searchText: text})}
-            style={{flex: 1, fontSize: 18, textAlignVertical: 'bottom', backgroundColor: 'transparent'}}
+            style={styles.searchTextInput}
           />
         </View>
-        <View style={styles.body}>
-          <ListView
-            data={products}
-            renderRow={this._renderRow}
-          />
-        </View>
+        <ScrollView style={styles.body}>
+          <ListView data={products} renderRow={this._renderRow} />
+        </ScrollView>
       </View>
     );
   }
+}
+
+const ListView = ({renderRow, data}) => {
+  return data.map(row => {
+    return renderRow(row);
+  });
 };
 
 const styles = StyleSheet.create({
@@ -104,6 +195,7 @@ const styles = StyleSheet.create({
     padding: 10,
     flex: 1,
     backgroundColor: '#fff',
+    // fontFamily: 'Open Sans',
   },
   header: {
     height: 100,
@@ -111,18 +203,42 @@ const styles = StyleSheet.create({
   },
   body: {
     flex: 1,
+    backgroundColor: '#fff',
+    height: '100%',
   },
   rowItemWraper: {
-    padding: 5,
-    marginBottom: 5,
-    borderWidth: 0.5,
-    borderColor: '#222',
     borderRadius: 5,
+    backgroundColor: '#fff',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    borderColor: '#8FD5A6',
+    borderWidth: 3,
+    marginBottom: 10,
   },
-  rowItem: {
-    flexShrink: 1,
-    backgroundColor: 'transparent'
-  }
+  rowItemInnerWraper: {
+    padding: 10,
+  },
+  productDescription: {
+    textAlign: 'left',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  searchTextInput: {
+    flex: 1,
+    fontSize: 18,
+    textAlignVertical: 'bottom',
+    backgroundColor: 'transparent',
+  },
+  brandOption: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 2,
+  },
+  brandOptionText: {
+    color: '#666',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
 });
 
 export default App;
